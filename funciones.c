2b71df1251c4e07804f47ad4d3aff20996a645c6
JoaquinYoko <jnegrete@frba.utn.edu.ar>
2023-05-16 18:44:08 -0300

    funcion 1 a

--- funciones.c
+++ funciones.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+
+int longitudCadena(char* cadena){
+    int contador = 0;
+
+    while (cadena[contador] != '\0'){
+        contador++;
+    }
+    return contador+1;
+}
+
+int main()
+{
+    char* cad = "hola";
+    int leng = longitudCadena(cad); 
+
+    printf("%d", leng);
+}
